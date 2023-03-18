5737dafc7d1fe0a972c8b5248a735814513a69cf
Johnny Wesley <johnnywesley.contato@outlook.com>
2023-03-18 00:03:50 -0300

    aula 2 : obstáculos

--- dino_runner/components/dinossaur.py
+++ dino_runner/components/dinossaur.py
@@ -27,19 +27,20 @@ class Dinosaur:
         if self.dino_duck:
             self.duck()
 
-        if user_input[pygame.K_DOWN] and not self.dino_jump:
-            self.dino_duck = True
-        else:
-            self.dino_duck = False
+
 
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
-
+            self.dino_duck = False
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_run = False
+            self.dino_jump = False
         elif not self.dino_jump and not self.dino_duck:
             self.dino_jump = False
-            self.dino_run = False
             self.dino_run = True
+            self.dino_duck = False
 
 
         if self.step_index >= 10:
@@ -69,7 +70,7 @@ class Dinosaur:
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS + 35
         self.step_index += 1
-
+        self.dino_duck = False
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
